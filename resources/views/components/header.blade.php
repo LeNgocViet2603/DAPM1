bfaf952ee7743ab449ce1d62a3af21e9ad015e31
benitdev <neihtben@gmail.com>
2022-06-17 14:45:42 +0700

    home layout

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,5 +1,51 @@
-<header class="header">
-    <div class="container">
-        
-    </div>  
+<header class="bg-green-600">
+    <div class="container mx-auto">
+        <!-- main-header -->
+        <div class="relative flex h-[8rem] items-center">
+            <a href="" class="bg-green-600 block w-[475px] border-logo h-full z-10">
+                <img src="https://antoanthucpham.danang.gov.vn/image/company_logo?img_id=10834&t=1655276002575" alt="logo" class="object-cover">
+            </a>
+            <div class="banner-slider h-full flex-1 overflow-hidden -ml-24">
+                <div>
+                    <img src="https://antoanthucpham.danang.gov.vn/documents/10181/10940/05.png" alt="" class="object-cover w-full h-full">
+                </div>
+                <div>
+                    <img src="https://antoanthucpham.danang.gov.vn/documents/10181/10940/06.png" alt="" class="object-cover w-full h-full">
+                </div>
+                <div>
+                    <img src="https://antoanthucpham.danang.gov.vn/documents/10181/10940/03.png" alt="" class="object-cover w-full h-full">
+                </div>
+            </div>
+            <div class="absolute bottom-0 right-0 bg-green-500/80 space-x-2 flex items-center">
+                <button class=" px-3 py-1 text-slate-900 font-bold text-sm"> Đăng Ký</button>
+                <span class="w-[1px] h-5 block bg-slate-900"></span>
+                <button class=" px-3 py-1 text-slate-900 font-bold text-sm"> Đăng Nhập</button>
+            </div>
+        </div>
+        <!-- menu-header -->
+        <ul class="flex">
+            <li class="flex-1 px-3 py-3 text-white font-bold uppercase cursor-pointer hover:bg-green-700 text-center">
+                trang chủ
+            </li>
+            <li class="flex-1 px-3 py-3 text-white font-bold uppercase cursor-pointer hover:bg-green-700 text-center">
+                giới thiệu
+            </li>
+            <li class="flex-1 px-3 py-3 text-white font-bold uppercase cursor-pointer hover:bg-green-700 text-center">
+                tin tức - sự kiện
+            </li>
+            <li class="flex-1 px-3 py-3 text-white font-bold uppercase cursor-pointer hover:bg-green-700 text-center">
+                thanh tra - kiểm tra
+            </li>
+            <li class="flex-1 px-3 py-3 text-white font-bold uppercase cursor-pointer hover:bg-green-700 text-center">
+                hệ thống - văn bản
+            </li>
+            <li class="flex-1 px-3 py-3 text-white font-bold uppercase cursor-pointer hover:bg-green-700 text-center">
+                truyền thông
+            </li>
+            <li class="flex-1 px-3 py-3 text-white font-bold uppercase cursor-pointer hover:bg-green-700 text-center">
+                chứng nhận ATTP
+            </li>
+        </ul>
+
+    </div>
 </header>
--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -1,22 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
     @yield('title')
-    <title>Document</title>
 </head>
 
 <body>
-    <div class="bg-slate-400">
-
+    <div>
         @include('components.header')
         <main class="container mx-auto">
             @yield('content')
         </main>
     </div>
-    
+    <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+    <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js">
+    </script>
+    <script>
+        $(document).ready(function() {
+            $('.banner-slider').slick({
+                infinite: true,
+                slidesToShow: 1,
+                arrows: false,
+                autoplay: true
+            })
+        })
+    </script>
 </body>
+
 </html>
